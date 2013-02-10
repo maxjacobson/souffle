43223f71c7a973d85e8a4a9ab9cbbba5265b4c6e
Max Jacobson <maxwell.jacobson@gmail.com>
2013-02-09 21:53:27 -0500

    made list items clickable

--- web.rb
+++ web.rb
@@ -4,12 +4,11 @@ require 'nokogiri'
 def xml_to_hash(xml)
   document = Nokogiri::XML(xml)
   elements = document.xpath("//outline/outline")
-  feeds = Hash.new
-  elements.each do |el|
-    title = el["title"]
-    feed = el["xmlUrl"]
-    feeds[feed] = title
+  feeds = Array.new
+  elements.each do |elem|
+    feeds.push({:title => elem["title"], :feed => elem["xmlUrl"]})
   end
+  puts feeds
   return feeds
 end
 
